d47a86115a7956bd45c58ed69f05e9d2a4b8aa29
moayed9919 <moayed9919@gmail.com>
2025-11-11 12:24:20 +0000

    add avatr options

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,8 +2,11 @@
 
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
-
 import { cn } from "./utils";
+
+// Import your avatar images
+import manAvatar from "../../assets/avatars/man_avatar.png"; 
+import womenAvatar from "../../assets/avatars/women_avatar.png"; 
 
 function Avatar({
   className,
@@ -23,12 +26,19 @@ function Avatar({
 
 function AvatarImage({
   className,
+  src,
+  gender = "male", // default gender if not provided
   ...props
-}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+}: React.ComponentProps<typeof AvatarPrimitive.Image> & { gender?: "male" | "female" }) {
+
+  // Select fallback based on gender
+  const fallbackSrc = src || (gender === "female" ? womenAvatar : manAvatar);
+
   return (
     <AvatarPrimitive.Image
       data-slot="avatar-image"
-      className={cn("aspect-square size-full", className)}
+      src={fallbackSrc}
+      className={cn("aspect-square size-full object-cover", className)}
       {...props}
     />
   );
